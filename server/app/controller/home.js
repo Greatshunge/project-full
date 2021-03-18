93d88fba3713ee8b726baaa13d333424f032d1ae
章顺 <shun.zhang@ilinklink.com>
2021-03-18 16:14:40 +0800

    update

--- server/app/controller/home.js
+++ server/app/controller/home.js
@@ -5,6 +5,7 @@ const Controller = require("egg").Controller;
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
+    console.log("index");
     ctx.body = "hi, egg";
   }
 }
